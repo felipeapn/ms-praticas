5974430862b1d355dd8ec240b3ceab95dd813119
felipeapn <felipeapn@yahoo.com.br>
2023-09-24 05:51:26 -0300

    Proyecto usuarios creado

---
+++ ms-praticas/src/main/java/com/praticas/mspraticas/MsPraticasApplication.java
@@ -0,0 +1,13 @@
+package com.praticas.mspraticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPraticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPraticasApplication.class, args);
+	}
+
+}
---
+++ ms-praticas/src/test/java/com/praticas/mspraticas/MsPraticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticas.mspraticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPraticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
