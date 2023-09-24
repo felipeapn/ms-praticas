23dbe771706580dfc6a04546a00d6097dbb776f9
felipeapn <felipeapn@yahoo.com.br>
2023-09-24 08:47:38 -0300

    Get usuriao por ID y docker conteiner

--- ms-usuario/src/main/java/com/praticas/msusuario/resources/UserBaseResources.java
+++ ms-usuario/src/main/java/com/praticas/msusuario/resources/UserBaseResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class UserBaseResources {
 	public ResponseEntity<List<UserBase>> findAll() {
 		List<UserBase> list = userBaseRepository.findAll();
 		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserBase> findById(@PathVariable Long id) {
+		UserBase userBase = userBaseRepository.findById(id).get();
+		return ResponseEntity.ok(userBase);
 	}
 
 }
